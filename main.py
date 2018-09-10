157e80ff249cd918d07f5e220e3c0fc6e0780b77
Jose Lorenzo <isleo11@gmail.com>
2018-09-10 09:52:08 +0100

    Primera version de ADV TTS

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+try: 
+    from BeautifulSoup import BeautifulSoup
+except ImportError:
+    from bs4 import BeautifulSoup
+from gtts import gTTS
+from time import sleep
+import os
+import pyglet
+
+url = 'https://ascodevida.com'
+r = requests.get(url)
+soup = BeautifulSoup(r.text)
+for adv in soup.findAll('a', {"class": "advlink"}):
+    print adv.text
+
+    tts = gTTS(text=adv.text, lang='es')
+    filename = '/tmp/temp.mp3'
+    tts.save(filename)
+
+    music = pyglet.media.load(filename, streaming=False)
+    music.play()
+
+    sleep(music.duration) #prevent from killing
+    os.remove(filename) #remove temperory file
